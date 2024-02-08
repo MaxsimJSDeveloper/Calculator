6c4d4809c2e2c221a2392f1c60d62c6c30c03392
MaxsimJSDeveloper <147420858+MaxsimJSDeveloper@users.noreply.github.com>
2024-02-08 12:00:17 +0200

    update0.1

---
+++ js/task-1.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const firstNumber = parseFloat(prompt('Input first number:'));
+const action = prompt('Input action (+, -, *, /):');
+const secondNumber = parseFloat(prompt('Input second number:'));
+
+function calc(a, b, callback) {
+  if (isNaN(Number(a)) || isNaN(Number(b))) {
+    if (action.length > 1) {
+      return alert(`Input correct number and action`);
+    }
+    return alert(`Input correct number!`);
+  }
+  switch (action) {
+    case '+':
+      return (callback = sum(a, b));
+    case '-':
+      return (callback = subtraction(a, b));
+    case '/':
+      return (callback = division(a, b));
+    case '*':
+      return (callback = multiplication(a, b));
+    default:
+      return alert('Input correct action (+ - * /)');
+  }
+}
+
+const sum = (a, b) => alert(`Sum ${a} and ${b}: ${a + b}`);
+const subtraction = (a, b) => alert(`Subtraction ${a} and ${b}: ${a - b}`);
+const division = (a, b) => alert(`Division ${a} and ${b}: ${a / b}`);
+const multiplication = (a, b) =>
+  alert(`Multiplication ${a} and ${b}: ${a * b}`);
+
+calc(firstNumber, secondNumber);
